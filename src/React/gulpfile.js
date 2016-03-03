a71320f3a6b73d78a57fcbef0ee8581357cbffd8
Paul Knopf <Paul Knopf>
2016-03-03 18:59:09 -0500

    Added support for watching dev or prod builds. "gulp watch build-dev" or "gulp watch build-prod" or "gulp watch build".

--- src/React/gulpfile.js
+++ src/React/gulpfile.js
@@ -2,11 +2,13 @@
 
 var gulp = require('gulp');
 var named = require('vinyl-named');
+var extend = require('lodash').extend;
 var webpackStream = require('webpack-stream');
 var webpackConfig = {
   dev : require("./Scripts/webpack/dev.config.js"),
   prod : require("./Scripts/webpack/prod.config.js")
 }
+var watch = false;
 
 var OUTPUT_DIR = './wwwroot/pack/';
 
@@ -18,14 +20,14 @@ gulp.task('build-dev', ['build-server-script-dev', 'build-client-script-dev']);
 gulp.task('build-server-script-dev', function() {
 	return gulp.src(['./Scripts/server.js'])
 		.pipe(named())
-		.pipe(webpackStream(webpackConfig.dev.server))
+		.pipe(webpackStream(extend({}, webpackConfig.dev.server, {watch:watch})))
 		.pipe(gulp.dest(OUTPUT_DIR));
 });
 
 gulp.task('build-client-script-dev', function() {
 	return gulp.src(['./Scripts/client.js'])
 		.pipe(named())
-		.pipe(webpackStream(webpackConfig.dev.client))
+		.pipe(webpackStream(extend({}, webpackConfig.dev.client, {watch:watch})))
 		.pipe(gulp.dest(OUTPUT_DIR));
 });
 
@@ -34,13 +36,17 @@ gulp.task('build-prod', ['build-server-script-prod', 'build-client-script-prod']
 gulp.task('build-server-script-prod', function() {
   return gulp.src(['./Scripts/server.js'])
     .pipe(named())
-    .pipe(webpackStream(webpackConfig.prod.server))
+    .pipe(webpackStream(extend({}, webpackConfig.prod.server, {watch:watch})))
     .pipe(gulp.dest(OUTPUT_DIR));
 });
 
 gulp.task('build-client-script-prod', function() {
   return gulp.src(['./Scripts/client.js'])
     .pipe(named())
-    .pipe(webpackStream(webpackConfig.prod.client))
+    .pipe(webpackStream(extend({}, webpackConfig.prod.client, {watch:watch})))
     .pipe(gulp.dest(OUTPUT_DIR));
+});
+
+gulp.task('watch', function() {
+  watch = true;
 });
