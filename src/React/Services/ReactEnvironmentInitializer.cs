d536fa3812f48985106e264b6be4a202e188716b
Paul Knopf <Paul Knopf>
2016-03-06 17:35:07 -0500

    Log errors if we couldn't initialize the react server-side environment.

--- src/React/Services/ReactEnvironmentInitializer.cs
+++ src/React/Services/ReactEnvironmentInitializer.cs
@@ -5,21 +5,31 @@ using System.Threading.Tasks;
 using JavaScriptViewEngine;
 using Microsoft.AspNet.Hosting;
 using System.IO;
+using Microsoft.Extensions.Logging;
 
 namespace React.Services
 {
     public class ReactEnvironmentInitializer : IJsEngineInitializer
     {
         private IHostingEnvironment _hostingEnvironment;
+        private ILogger _logger;
 
-        public ReactEnvironmentInitializer(IHostingEnvironment hostingEnvironment)
+        public ReactEnvironmentInitializer(IHostingEnvironment hostingEnvironment, ILoggerFactory loggerFactory)
         {
             _hostingEnvironment = hostingEnvironment;
+            _logger = loggerFactory.CreateLogger<ReactEnvironmentInitializer>();
         }
 
         public void Initialize(IJsEngine engine)
         {
-            engine.ExecuteFile(Path.Combine(_hostingEnvironment.WebRootPath, "pack", "server.generated.js"));
+            try
+            {
+                engine.ExecuteFile(Path.Combine(_hostingEnvironment.WebRootPath, "pack", "server.generated.js"));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Could not initialize the React server-side environment.", ex);
+            }
         }
     }
 }
