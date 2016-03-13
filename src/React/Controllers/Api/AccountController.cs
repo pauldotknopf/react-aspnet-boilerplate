fee64236a2b38d4c73a7e296be7db729cafb8caf
Paul Knopf <Paul Knopf>
2016-03-13 16:52:48 -0400

    Added the api endpoint for registering a user. The response may need to change (for validation). Will ironing that out soon.

---
+++ src/React/Controllers/Api/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Mvc;
+using Newtonsoft.Json.Linq;
+using React.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace React.Controllers.Api
+{
+    [Route("api/account")]
+    public class AccountController : Controller
+    {
+        UserManager<ApplicationUser> _userManager;
+        SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+        }
+
+        [Route("register")]
+        [HttpPost]
+        public async Task<object> Register([FromBody]RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = model.UserName, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return new
+                    {
+                        succes = true
+                    };
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return new
+            {
+                errors = ModelState.Values
+            };
+        }
+    }
+}
